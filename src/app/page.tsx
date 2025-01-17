2b74b67565192d09b53db58b93e22ef9bb71087c
Ruan Matos <ruansanttosmattos02@gmail.com>
2025-01-17 12:28:56 -0300

    feat(Home Page): criação de validação do formulário de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,61 @@
-import styles from "../styles/page.module.css";
+'use client'
+import * as yup from 'yup'
+import styles from "../styles/home.module.css";
+import { FormEvent, useState } from "react";
+import { ILogin } from '@/types/Home';
 
 export default function Home() {
+
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  function onchangeEmail(email: string) {
+    setEmail(email)
+  }
+
+  function onChangePassword(password: string) {
+    setPassword(password)
+  }
+
+  function alertInvalidFields(id: string, message: string) {
+    const alert = document.getElementById(id) as HTMLElement
+    alert.innerHTML = message
+  }
+
+  function disableAlertFields(id: string) {
+    const alert = document.getElementById(id) as HTMLElement
+    alert.innerHTML = ''
+  }
+
+  function handleSubmitForm(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    validantionFields()
+  }
+
+  function validantionFields() {
+    try {
+      const schema: yup.ObjectSchema<ILogin> = yup.object().shape(
+        {
+          email: yup.string().required().email(),
+          password: yup.string().required().min(6)
+        }
+      )
+
+      schema.validateSync({ email, password }, { abortEarly: false })
+      schema.type == 'object' && alert('tudo certo')
+    }
+    catch (error) {
+      const yupErro = error as yup.ValidationError
+      const erroValidation: Record<string, string> = {}
+
+      yupErro.inner.forEach(erro => {
+        if (erro.path == undefined) { return }
+        erroValidation[erro.path] = erro.message
+        alertInvalidFields(`alert-${erro.path}`, erro.message)
+      })
+    }
+  }
+
   return (
     <main className={styles.main}>
       <section className={styles.container}>
@@ -8,16 +63,21 @@ export default function Home() {
           <h1>Login</h1>
           <p>Insira seus dados para acessar o sistema</p>
         </div>
-        <form action="" method="POST" className={styles.form}>
+        <form 
+          className={styles.form}
+          onSubmit={(e) => handleSubmitForm(e)}
+        >
           <div className={styles.inputArea}>
-            <label htmlFor="login">Email</label>
+            <label htmlFor="email">Email</label>
             <input
               type="email"
-              name="login"
+              name="email"
               placeholder="exampleUser@gmail.com"
               className={styles.txtInput}
+              onChange={(e) => onchangeEmail(e.target.value)}
+              onFocus={() => disableAlertFields('alert-email')}
             />
-            <span id="alert-login" className={styles.alertText}></span>
+            <span id="alert-email" className={styles.alertText}></span>
           </div>
           <div className={styles.inputArea}>
             <label htmlFor="password">Senha</label>
@@ -25,6 +85,8 @@ export default function Home() {
               type="password"
               name="password"
               className={styles.txtInput}
+              onChange={(e) => onChangePassword(e.target.value)}
+              onFocus={() => disableAlertFields('alert-password')}
             />
             <span id="alert-password" className={styles.alertText}></span>
           </div>
