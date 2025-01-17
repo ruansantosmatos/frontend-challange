3bfad7c82607885ed1e6f09ef97600cee4df44d1
Ruan Matos <ruansanttosmattos02@gmail.com>
2025-01-17 00:31:16 -0300

    feat(Home Page): criação de funcionalidade de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,45 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../styles/page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main className={styles.main}>
+      <section className={styles.container}>
+        <div className={styles.header}>
+          <h1>Login</h1>
+          <p>Insira seus dados para acessar o sistema</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <form action="" method="POST" className={styles.form}>
+          <div className={styles.inputArea}>
+            <label htmlFor="login">Email</label>
+            <input
+              type="email"
+              name="login"
+              placeholder="exampleUser@gmail.com"
+              className={styles.txtInput}
+            />
+            <span id="alert-login" className={styles.alertText}></span>
+          </div>
+          <div className={styles.inputArea}>
+            <label htmlFor="password">Senha</label>
+            <input
+              type="password"
+              name="password"
+              className={styles.txtInput}
+            />
+            <span id="alert-password" className={styles.alertText}></span>
+          </div>
+          <div className={styles.forgetPassword}>
+            <a href="/" target="_self">
+              Esqueceu a senha?
+            </a>
+          </div>
+          <div className={styles.containerBtn}>
+            <button type="submit" className={styles.btnSubmit}>
+              Entrar
+            </button>
+          </div>
+        </form>
+      </section>
+    </main>
+  )
 }
